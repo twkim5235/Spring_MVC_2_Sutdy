ab22d30768dda222d316da56c70fbcbf5e41e548
twkim5235 <sssunggure@naver.com>
2022-03-30 23:31:57 +0900

    Thymleaf basic project inital

---
+++ thymleaf-basic/src/test/java/hello/thymleaf/ThymleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
