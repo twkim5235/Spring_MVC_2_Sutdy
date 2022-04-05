890f9be9e1a02642e122744de13a4fc67204d901
twkim5235 <sssunggure@naver.com>
2022-04-05 15:37:36 +0900

    [ADD] 자바스크립트 인라인 내용 추가

--- thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
+++ thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
@@ -114,10 +114,18 @@ public class BasicController {
         return "basic/comments";
     }
 
-    @GetMapping("block")
+    @GetMapping("/block")
     public String block(Model model) {
         addUsers(model);
         return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("\"userA\"", 10));
+        addUsers(model);
+
+        return "basic/javascript";
     }
 
     private void addUsers(Model model) {
