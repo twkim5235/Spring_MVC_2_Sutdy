7a79c48a52090e29b3f2ac2a435b0bdc3d90408c
twkim5235 <sssunggure@naver.com>
2022-04-13 23:05:01 +0900

    [ADD] 세션 직접 구현 및 테스트

---
+++ login/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,74 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션 관리
+ */
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    /**
+     * 세션 생성
+     *  sessionId 생성
+     *  session 저장소에 값 저장
+     *  쿠키에 sessionId를 담아서 응답
+     */
+    public void createSession(Object value, HttpServletResponse response) {
+        //세션 Id 생성
+        String sessionId = UUID.randomUUID().toString();
+        //세션에 값 저장
+        sessionStore.put(sessionId, value);
+
+        //쿠키 생성
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+    }
+
+    /**
+     * 세션 조회
+     */
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
+
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+}
---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,42 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class SessionManagerTest {
+
+    @Autowired
+    SessionManager sessionManager;
+
+    @Test
+    void sessionTest() {
+
+        //세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies()); //mySessionId = 123123123123
+
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+}
