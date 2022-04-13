20baf953c7fc0aaa9da868c11764a79c9d89484a
twkim5235 <sssunggure@naver.com>
2022-04-13 19:12:35 +0900

    [ADD] 로그인 기능 구현

--- message/src/main/java/hello/itemservice/Service/RetryableService.java
+++ message/src/main/java/hello/itemservice/Service/RetryableService.java
@@ -1,9 +1,14 @@
 package hello.itemservice.Service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
@@ -21,5 +26,24 @@ public class RetryableService {
             throw new RuntimeException();
         }
         throw new IllegalStateException();
+    }
+
+    @Async(value = "asyncThread")
+    public void asyncMethod(int i) {
+        try {
+            log.info("[시작]AsyncThread = " + i + " " + Thread.currentThread().getName());
+//            Thread.sleep(500);
+            log.info("[종료]AsyncThread = " + i + " " + Thread.currentThread().getName());
+        } catch (Exception e) {
+
+        }
+
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+    }
+
+    @Bean(name = "asyncThread")
+    public Executor asyncThread() {
+        return Executors.newFixedThreadPool(8);
     }
 }
--- message/src/test/java/hello/itemservice/Service/RetryableServiceTest.java
+++ message/src/test/java/hello/itemservice/Service/RetryableServiceTest.java
@@ -1,4 +1,25 @@
+package hello.itemservice.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@EnableAsync
 class RetryableServiceTest {
-  
+
+    @Autowired
+    RetryableService retryableService;
+
+    @Test
+    public void Test() throws InterruptedException {
+        for (int i = 0; i < 50; i++) {
+            retryableService.asyncMethod(i);
+        }
+        Thread.sleep(1000);
+    }
 }
